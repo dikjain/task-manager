1a46ef7b921b1e19d518f86b514706feca9c5c4b
dikjain <dikshitmahanot2005@gmail.com>
2025-02-02 21:37:32 +0530

    chnges

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import SideBar from "./components/SideBar";
 import { ClerkProvider } from "@clerk/nextjs";
 export const metadata: Metadata = {
   title: "TaskMaster",
